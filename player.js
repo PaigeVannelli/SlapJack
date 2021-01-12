85e605974b9b07741afdc73bb825c4308ac9d8b1
Paige Vannelli <paigevannelli@gmail.com>
2021-01-12 15:18:48 -0700

    Create player class

---
+++ player.js
@@ -0,0 +1,10 @@
+class Player {
+  constructor(id) {
+    this.id = id;
+    this.wins = 0;
+    this.hand = [];
+  }
+  saveWinsToStorage() {
+    //This should take this.wins and save to local storage 
+  }
+}
