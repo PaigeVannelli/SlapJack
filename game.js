6e931870d8e84f44613bcf289530856038d3dc93
Paige Vannelli <paigevannelli@gmail.com>
2021-01-15 16:33:55 -0700

    Refactor slapCard function

--- game.js
+++ game.js
@@ -73,74 +73,115 @@ class Game {
 
   slapCard(player) {
     if (!this.endGame) {
-      if (this.centerPile.length > 0 && this.centerPile[0].includes("jack")) {
-        console.log("Jack")
-        this.winPile(player)
-      } else if (this.centerPile.length > 1 && this.centerPile[0].slice(-2) === this.centerPile[1].slice(-2)) {
-        console.log("Double")
-        this.winPile(player)
-      } else if (this.centerPile.length > 2 && this.centerPile[0].slice(-2) === this.centerPile[2].slice(-2)) {
-        console.log("Sandwich")
-        this.winPile(player)
-      } else {
-        console.log("bad slap")
-        this.losePile(player)
-      }
+      this.normalPlay(player)
     } else if (this.endGame === "player1") {
-      // console.log("endGame", this.endGame)
-      // console.log("includes jack", this.centerPile[0].includes("jack"), this.centerPile.length > 0)
-      if (this.centerPile.length > 0 && this.centerPile[0].includes("jack")) {
-        console.log("Jack")
-        if (player === this.player1) {
-          this.winPile(this.player1)
-          this.endGame = false
-        } else if (player === this.player2) {
-          console.log("player 2 wins!")
-        }
-      } else {
-        console.log("bad slap")
-        if (player === this.player1) {
-          console.log("player 2 wins")
-          // this.endGame = false
-          // Reset the deck
-        } else if (player === this.player2) {
-          this.winPile(this.player1);
-          this.endGame = false
-        }
-      }
+      // if (this.player2.hand.length > 0) {
+      //   if (this.centerPile.length > 0 && this.centerPile[0].includes("jack")) {
+      //     console.log("Jack")
+      //     if (player === this.player1) {
+      //       this.winPile(this.player1)
+      //       this.endGame = false
+      //     } else if (player === this.player2) {
+      //       console.log("player 2 wins!")
+      //     }
+      //   } else {
+      //     console.log("bad slap")
+      //     if (player === this.player1) {
+      //       console.log("player 2 wins")
+      //       // this.endGame = false
+      //       // Reset the deck
+      //     } else if (player === this.player2) {
+      //       this.winPile(this.player1);
+      //       this.endGame = false
+      //     }
+      //   }
+      // } else if (this.player2.hand.length === 0) {
+      //   console.log("player 2 out of cards", this.centerPile)
+      //   this.player2.hand.push(this.centerPile.shuffle())
+      //   console.log("after shuffle", this.centerPile)
+      // }
+      this.endGamePlay(player, this.player1, this.player2);
+
     } else if (this.endGame === "player2") {
-      console.log("endGame2", this.endGame)
-      if (this.centerPile.length > 0 && this.centerPile[0].includes("jack")) {
-        console.log("Jack")
-        if (player === this.player2) {
-          this.winPile(this.player2)
-          this.endGame = false
-        } else if (player === this.player1) {
-          console.log("player 1 wins!")
-        }
-      } else {
-        console.log("bad slap")
-        if (player === this.player2) {
-          console.log("player 1 wins")
-          // this.endGame = false
-          // Reset the deck
-        } else if (player === this.player1) {
-          this.winPile(this.player2);
-          this.endGame = false
-        }
-      }
-
-    //We want only to check for jacks
-    //if there is a jack we need to figure out which player slapped it
-    // We are passing in which player slapped in our event listener
-    //if it's the loser they winCards()
-    //if it's the winner the game ends and they win a point
-
-    //if no jack is slapped and the winners array hits 0
-    //the winner then winsCards, end game is still going and they keep playing
-
+      // if (this.player1.hand.length > 0) {
+      //   console.log("endGame2", this.endGame)
+      //   if (this.centerPile.length > 0 && this.centerPile[0].includes("jack")) {
+      //     console.log("Jack")
+      //     if (player === this.player2) {
+      //       this.winPile(this.player2)
+      //       this.endGame = false
+      //     } else if (player === this.player1) {
+      //       console.log("player 1 wins!")
+      //     }
+      //   } else {
+      //     console.log("bad slap")
+      //     if (player === this.player2) {
+      //       console.log("player 1 wins")
+      //       // this.endGame = false
+      //       // Reset the deck
+      //     } else if (player === this.player1) {
+      //       this.winPile(this.player2);
+      //       this.endGame = false
+      //     }
+      //   }
+      // //   else if ()
+      // // //if no jack is slapped and the winners array hits 0
+      // // //the winner then winsCards, end game is still going and they keep playing
+      // } else {
+      //   console.log("player 1 out of cards", this.centerPile)
+      //   this.player1.hand.push(this.centerPile.shuffle())
+      //   console.log("after shuffle", this.centerPile)
+      // }
+      this.endGamePlay(player, this.player2, this.player1);
     }
   }
+
+ normalPlay(player) {
+   if (this.centerPile.length > 0 && this.centerPile[0].includes("jack")) {
+     console.log("Jack")
+     this.winPile(player)
+   } else if (this.centerPile.length > 1 && this.centerPile[0].slice(-2) === this.centerPile[1].slice(-2)) {
+     console.log("Double")
+     this.winPile(player)
+   } else if (this.centerPile.length > 2 && this.centerPile[0].slice(-2) === this.centerPile[2].slice(-2)) {
+     console.log("Sandwich")
+     this.winPile(player)
+   } else {
+     console.log("bad slap")
+     this.losePile(player)
+   }
+  }
+
+  endGamePlay(player, currentLoser, currentWinner) {
+    if (currentWinner.hand.length > 0) {
+      if (this.centerPile.length > 0 && this.centerPile[0].includes("jack")) {
+        // console.log("Jack")
+        if (player === currentLoser) {
+          this.winPile(currentLoser)
+          this.endGame = false
+          console.log(`${currentLoser} gets the cards`, currentLoser.hand)
+        } else if (player === currentWinner) {
+          console.log(`${currentWinner} wins`)
+        }
+      } else {
+        console.log("bad slap")
+        if (player === this.currentLoser) {
+          console.log(`${currentWinner} wins`)
+          // this.endGame = false
+          // Reset the deck
+        } else if (player === this.currentWinner) {
+          this.winPile(this.currentLoser);
+          this.endGame = false
+        }
+      }
+    }
+  }
+  // if (this.player1.hand.length === 0)
+  //We want only to check for jacks
+  //if there is a jack we need to figure out which player slapped it
+  // We are passing in which player slapped in our event listener
+  //if it's the loser they winCards()
+  //if it's the winner the game ends and they win a point
 
   winPile(player) {
     var wonCards = this.shuffle(this.centerPile)
